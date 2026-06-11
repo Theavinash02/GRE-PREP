f03d3c5bb775151818d17720819b08ff6d06032e
Avinash <103890061+Theavinash02@users.noreply.github.com>
2026-06-11 12:53:18 +0530

    Uploaded the files

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'gre-vocab-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./gre_vocab.html','./manifest.json']))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res;
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => caches.match('./gre_vocab.html'));
+    })
+  );
+});
